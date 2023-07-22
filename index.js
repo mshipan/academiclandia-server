c0ec5c7f0d1caa013bd54aa1f973fd4b7af9764a
mshipan <mshipan657@gmail.com>
2023-07-23 00:07:18 +0600

    server setup and create colleges collection

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//mongodb start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1oh7p7d.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //collections
+    const collegeCollection = client
+      .db("academiclandiaDB")
+      .collection("colleges");
+
+    // api start
+    app.get("/colleges", async (req, res) => {
+      const result = await collegeCollection.find().toArray();
+      res.send(result);
+    });
+    // api end
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//mongodb end
+
+// basic setup
+app.get("/", (req, res) => {
+  res.send("Academiclandia Server is Running.");
+});
+
+app.listen(port, () => {
+  console.log(`Academiclandia Server is Running on port: ${port}`);
+});
