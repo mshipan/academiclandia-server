c8f2e76d8c8c875f1d403c342153fbc07baf71bc
mshipan <mshipan657@gmail.com>
2023-07-23 20:59:14 +0600

    create single college details apis

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -45,6 +45,13 @@ async function run() {
     //college apis
     app.get("/colleges", async (req, res) => {
       const result = await collegeCollection.find().toArray();
+      res.send(result);
+    });
+    //view single college details
+    app.get("/college/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await collegeCollection.findOne(query);
       res.send(result);
     });
     // gallery apis
